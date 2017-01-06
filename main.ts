b0b0977bb3c42be3f05364537873b9ec4b99f0a3
Joilson Nascimento <joilsoncisne@gmail.com>
2017-01-06 21:29:17 -0200

    Use merge operator with throw

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
 import {Observable} from 'rxjs';
 import {load, loadWithFetch} from "./loaders";
 
-let source = Observable.create(observer => {
-    observer.next(1);
-    observer.next(2);
-    observer.error('Stop!');
-    // throw new Error('Stop!');
-    observer.next(2);
-    observer.complete();
-});
+let source = Observable.merge(
+    Observable.of(1),
+    Observable.from([2, 3, 4]),
+    Observable.throw(new Error('Stop!')),
+    Observable.of(5)
+);
 
 source.subscribe(
     value => console.log(`value: ${value}`),
