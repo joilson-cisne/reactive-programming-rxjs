5f1a4da7976adacc505258baa6ee3e999cf53d00
Joilson Nascimento <joilsoncisne@gmail.com>
2017-01-06 18:20:38 -0200

    Add simple Observable and Observer

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
-alert('ok');
+import {Observable} from 'rxjs';
+
+let numbers = [10, 20, 30];
+let source = Observable.from(numbers);
+
+class MyObservable {
+    next(value) {
+        console.log(`value: ${value}`);
+    }
+
+    error(e) {
+        console.log(`error: ${e}`);
+    }
+
+    complete() {
+        console.log('complete');
+    }
+}
+
+source.subscribe(new MyObservable());
