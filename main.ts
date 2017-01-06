e79498e48f092621a6124b1fd92bf9fc103f2e87
Joilson Nascimento <joilsoncisne@gmail.com>
2017-01-06 18:58:47 -0200

    Import all operators for dev purposes

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-// import {Observable} from 'rxjs';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
+import {Observable} from 'rxjs';
+
+// import {Observable} from 'rxjs/Observable';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/filter';
 
 let numbers = [10, 20, 40];
 
