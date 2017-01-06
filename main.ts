544089ba0ee4fe631dcb075807824b94a3bb95b7
Joilson Nascimento <joilsoncisne@gmail.com>
2017-01-06 18:27:04 -0200

    Remove MyObserver class

--- main.ts
+++ main.ts
@@ -6,18 +6,8 @@ import {
 let numbers = [10, 20, 30];
 let source = Observable.from(numbers);
 
-class MyObservable implements Observer<number> {
-    next(value) {
-        console.log(`value: ${value}`);
-    }
-
-    error(e) {
-        console.log(`error: ${e}`);
-    }
-
-    complete() {
-        console.log('complete');
-    }
-}
-
-source.subscribe(new MyObservable());
+source.subscribe(
+    value => console.log(`value: ${value}`),
+    err => console.log(`error: ${err}`),
+    () => console.log('complete')
+);
